9dc51d09048834bfe8ce98400e7cc8bfcc35df1c
Anand  Sarolkar <98941524+Riseonelimit@users.noreply.github.com>
2024-11-29 22:31:31 +0530

    Bug fix (#3)
    UContextFix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,36 @@
-import { useContext, useEffect } from 'react'
-import Navbar from './components/Navbar.jsx'
-import Home from './pages/Home.jsx'
-import { Outlet, useLocation } from 'react-router-dom'
-import UContext from './context/UContext'
-import { authLogin } from './api/auth'
+import { useContext, useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { authLogin } from "./api/auth";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import { UContextProvider } from "./context/UContext.jsx";
 
 function App() {
-  const context = useContext(UContext)
-  const {pathname} = useLocation()
-  
-  useEffect(() => {
-    const auth = async () => {
-      const userData = await authLogin(localStorage.getItem('accessToken'))
-      if (userData) {
-        context.setUser(userData)
-      }else {
-        context.setUser(null)
-      }
-    }
-    auth()
-  },[])
-  return (
-    <section className=' w-full flex-box flex-col'>
-      <Navbar />
-      {pathname == '/'  ?  <Home/>: <Outlet/>}
-    </section>
-  )
+    const { user, setUser } = useContext(UContextProvider);
+    const { pathname } = useLocation();
+    console.log(user);
+
+    useEffect(() => {
+        const auth = async () => {
+            const userData = await authLogin(
+                localStorage.getItem("accessToken")
+            );
+            if (userData) {
+                setUser(userData);
+            } else {
+                setUser(null);
+            }
+        };
+        auth();
+    }, [setUser]);
+    return (
+        <section className=" w-full flex-box flex-col">
+            <Navbar />
+            <h1 className="text-xl">{user.name}</h1>
+            {user ? "Hie" : "byee "}
+            {pathname == "/" ? <Home /> : <Outlet />}
+        </section>
+    );
 }
 
-export default App
+export default App;
