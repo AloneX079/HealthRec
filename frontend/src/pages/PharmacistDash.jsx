12e51f4a97b04d90927b898ffa7cb3565e199903
Alone_X <62175217+AloneX079@users.noreply.github.com>
2024-12-02 22:43:45 +0530

    Pharmacy Panel
    -Working Pharmacist Dashboard

--- frontend/src/pages/PharmacistDash.jsx
+++ frontend/src/pages/PharmacistDash.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../components/Footer";
 import QRCode from "react-qr-code";
 import useUserContext from "../hooks/useUserContext";
@@ -8,17 +8,31 @@ import { getPatientPrescription } from "../api/POST";
 function PharmacistDash() {
   const [selectedItem, setSelectedItem] = useState("Doctor QR");
   const [patientList, setPatientList] = useState([]);
-  const { user, setUser, loading, setLoading } = useUserContext();
+  const [patientPrescription, setPatientPrescription] = useState({});
+  const { user, loading, setLoading } = useUserContext();
 
   const fetchPatientList = async () => {
     try {
+      setLoading(true);
       const patientListResponse = await getPatientList();
       if (patientListResponse.data.success) {
-        setPatientList(patientListResponse.data.data);
-        patientList.map((patient, index) => {
-          const patientpres = getPatientPrescription(patient.patient);
-          console.log(patientpres);
-        });
+        const fetchedPatientList = patientListResponse.data.data;
+        setPatientList(fetchedPatientList);
+        const prescriptions = {};
+
+        for (const patient of fetchedPatientList) {
+          const patientpres = await getPatientPrescription(patient.patient);
+          if (patientpres.success && patientpres.data) {
+            prescriptions[patient.patient] = patientpres.data;
+          } else {
+            console.error(
+              `Failed to fetch prescription for ${patient.patient}`
+            );
+          }
+        }
+
+        setPatientPrescription(prescriptions);
+        console.log(patientPrescription);
       } else {
         console.error(
           "Failed to fetch patient list:",
@@ -27,68 +41,57 @@ function PharmacistDash() {
       }
     } catch (error) {
       console.error("Error fetching patient list:", error);
+    } finally {
+      setLoading(false);
     }
   };
-
+  //This function made me cry fr
   const content = patientList.reduce((acc, patient) => {
-    acc[patient.patientName] = "test";
-    //   <div className="p-6 bg-white rounded-lg shadow-md">
-    //     <div className="flex justify-between items-center mb-6">
-    //       <h2 className="text-2xl font-semibold text-green-900">
-    //         Basic Information
-    //       </h2>
-    //     </div>
-    //     <div className="space-y-4">
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Full Name:
-    //         </span>
-    //         <span className="text-lg">{record?.fullName}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Date of Birth:
-    //         </span>
-    //         <span className="text-lg">{record?.dateOfBirth}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Blood Group:
-    //         </span>
-    //         <span className="text-lg">{record?.bloodGroup}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Phone Number:
-    //         </span>
-    //         <span className="text-lg">{record?.phoneNumber}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">Address:</span>
-    //         <span className="text-lg">{record?.address}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Marital Status:
-    //         </span>
-    //         <span className="text-lg">{record?.maritalStatus}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Ethnicity / Race:
-    //         </span>
-    //         <span className="text-lg">{record?.ethnicityRace}</span>
-    //       </div>
-    //       <div className="flex justify-between">
-    //         <span className="text-lg font-medium text-green-800">
-    //           Smoking / Alcohol:
-    //         </span>
-    //         <span className="text-lg">{record?.smokingAlcohol}</span>
-    //       </div>
-    //     </div>
-    //   </div>
+    acc[patient.patientName] = (
+      <div className="p-6 bg-white rounded-lg shadow-md">
+        <div className="flex justify-between items-center mb-6">
+          <h2 className="text-2xl font-semibold text-green-900">
+            Visit History
+          </h2>
+        </div>
+        <div className="space-y-4">
+          <div
+            className="h-80 overflow-y-auto border border-gray-300 rounded-lg p-6"
+            style={{ maxHeight: "256px" }}
+          >
+            {patientPrescription[patient.patient]?.data ? (
+              patientPrescription[patient.patient].data.map((item, index) => (
+                <div
+                  key={index}
+                  className="mb-4 p-4 bg-gray-100 rounded-lg shadow-sm"
+                >
+                  <p className="text-lg font-medium text-green-800">
+                    Doctor: <span className="text-gray-800">{item.doctor}</span>
+                  </p>
+                  <p className="text-lg font-medium text-green-800">
+                    Illness:{" "}
+                    <span className="text-gray-800">{item.illness}</span>
+                  </p>
+                  <p className="text-lg font-medium text-green-800">
+                    Prescription:{" "}
+                    <span className="text-gray-800">{item.prescription}</span>
+                  </p>
+                  <p className="text-lg font-medium text-green-800">
+                    Created At:{" "}
+                    <span className="text-gray-800">{item.createdAt}</span>
+                  </p>
+                </div>
+              ))
+            ) : (
+              <p className="text-center text-gray-600">No records available.</p>
+            )}
+          </div>
+        </div>
+      </div>
+    );
     return acc;
   }, {});
+
   useEffect(() => {
     fetchPatientList();
   }, []);
@@ -110,23 +113,23 @@ function PharmacistDash() {
               >
                 Pharmacist QR
               </li>
-              {patientList.map((patient, index) => (
+              {patientList?.map((patient, index) => (
                 <li
                   key={index}
-                  onClick={() => setSelectedItem(patient.patientName)}
+                  onClick={() => setSelectedItem(patient?.patientName)}
                   className={`cursor-pointer px-4 py-3 text-lg font-medium text-green-800 hover:bg-green-400 duration-300 hover:text-white rounded-xl ${
-                    selectedItem === patient.patientName
+                    selectedItem === patient?.patientName
                       ? "bg-green-500 text-white"
                       : ""
                   } border-b-2 border-green-400`}
                 >
-                  {patient.patientName}
+                  {patient?.patientName}
                 </li>
               ))}
             </ul>
           </div>
-          <div className="w-3/4 p-6 flex items-center justify-center text-green-900">
-            <div className="text-lg font-medium w-2/4 h-2/4">
+          <div className="w-3/4 p-3 flex items-center justify-center text-green-900">
+            <div className="text-lg font-medium w-3/4 h-2/4">
               {selectedItem === "Doctor QR" ? (
                 <div className="flex flex-col items-center justify-center w-full h-full">
                   {loading ? (
